b4bdf116ee795ba48f999ebc1407d03f01f5c5b8
Brendon Butler <bbutler452@gmail.com>
2015-12-20 20:09:44 -0500

    Let's get this started!

---
+++ src/net/sparkzz/game/Game.java
@@ -0,0 +1,11 @@
+package net.sparkzz.game;
+
+/**
+ * Created by Brendon Butler on 12/20/2015.
+ */
+public class Game {
+
+	public static void main(String[] args) {
+		System.out.println("Let's get this started!");
+	}
+}
