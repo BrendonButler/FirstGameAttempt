21731d16a179b9c113f817054bf032ba04c178ef
Brendon Butler <bbutler452@gmail.com>
2015-12-20 21:02:56 -0500

    Added randomness and responses

--- src/net/sparkzz/game/Octoball.java
+++ src/net/sparkzz/game/Octoball.java
@@ -3,25 +3,41 @@ package net.sparkzz.game;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Brendon Butler on 12/20/2015.
  */
 public class Octoball {
 
-	BufferedReader reader;
+	private BufferedReader reader;
+	private Random random;
+	private List<String> answers = new ArrayList<>();
 
 	public Octoball() {
 		reader = new BufferedReader(new InputStreamReader(System.in));
+		random = new Random();
+
+		answers.add("It is certain");
+		answers.add("Yes");
+		answers.add("No");
+		answers.add("Ask again later");
+		answers.add("Very doubtful");
 	}
 
 	public void prompt() throws IOException {
 		System.out.printf("%nWhat is your question?%n> ");
 		String input = reader.readLine();
 
-		System.out.printf("Your input: %s%n%n", input);
-
-		if (input.equalsIgnoreCase("quit"))
+		if (input.equalsIgnoreCase("quit")) {
 			Game.stop();
+			return;
+		}
+
+		int i = random.nextInt(5);
+
+		System.out.println(answers.get(i));
 	}
 }
